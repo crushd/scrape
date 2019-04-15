7956fed72d863244f7856c347ff7190b9df6bc50
Cody Rush <codyrush@gmail.com>
2019-04-15 11:42:27 -0700

    setting up Movie model

--- models/Movie.js
+++ models/Movie.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+var MovieSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
@@ -15,18 +15,18 @@ var ArticleSchema = new Schema({
   link: {
     type: String,
     required: true
-  },
+  }
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+  // note: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "Note"
+  // }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Movie = mongoose.model("Movie", MovieSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = Movie;
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/apple", {
+mongoose.connect("mongodb://localhost/openingmovies", {
   useNewUrlParser: true
 });
 
@@ -37,7 +37,7 @@ mongoose.connect("mongodb://localhost/apple", {
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
   axios
-    .get("https://www.apple.com/shop/refurbished/mac/macbook-air")
+    .get("https://www.rottentomatoes.com/browse/opening/")
     .then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
@@ -45,25 +45,25 @@ app.get("/scrape", function(req, res) {
       //console.log(response.data);
 
       // Now, we grab every h3 within a div class .as-producttile-info tag, and do the following:
-      $(".as-producttile-info h3").each(function(i, element) {
+      $(".mb-movie h3").each(function(i, element) {
         // Save an empty result object
         var result = {};
 
-        console.log(this);
+        console.log(element);
 
         // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
+        result.title = $(this).text();
         result.link = $(this)
           .children("a")
           .attr("href");
 
+        console.log(result);
+
         // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
+        db.Movie.create(result)
+          .then(function(dbMovie) {
             // View the added result in the console
-            console.log(dbArticle);
+            console.log(dbMovie);
           })
           .catch(function(err) {
             // If an error occurred, log it
@@ -77,12 +77,12 @@ app.get("/scrape", function(req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/movies", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
+  db.Movie.find({})
+    .then(function(dbMovie) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.json(dbMovie);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -91,14 +91,14 @@ app.get("/articles", function(req, res) {
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/movies/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  db.Movie.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
+    // .populate("note")
+    .then(function(dbMovie) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
+      res.json(dbMovie);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -107,28 +107,28 @@ app.get("/articles/:id", function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+// app.post("/movies/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Article.findOneAndUpdate(
+//         { _id: req.params.id },
+//         { note: dbNote._id },
+//         { new: true }
+//       );
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
 // Start the server
 app.listen(PORT, function() {
